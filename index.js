62e20a02e67aa150bb3a7a182028e0727017fe22
isotop786 <marufislam77@gmail.com>
2023-08-22 20:15:27 +0600

    client host updated

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const leaveRoom = require('./utils/leave-room');
 
 const io = new Server(server,{
     cors:{
-        origin:'http://localhost:3000',
+        // origin:'http://localhost:3000',
+        origin:'https://reactjs-socket-io.vercel.app',
         methods:['GET','POST'],
     }
 })
